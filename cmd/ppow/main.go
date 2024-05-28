f2e87e99bd9852bdb12f267010c0c903f9f1bc31
Mikhail Gusarov <dottedmag@dottedmag.net>
2024-05-28 09:42:19 +0200

    Replace kingping with (smaller) pflag

--- cmd/ppow/main.go
+++ cmd/ppow/main.go
@@ -7,48 +7,25 @@ import (
 	"github.com/cortesi/termlog"
 	"github.com/dottedmag/ppow"
 	"github.com/dottedmag/ppow/notify"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/spf13/pflag"
 )
 
-const modfile = "./ppow.conf"
-
-var file = kingpin.Flag(
-	"file",
-	fmt.Sprintf("Path to modfile (%s)", modfile),
-).
-	Default(modfile).
-	PlaceHolder("PATH").
-	Short('f').
-	String()
-
-var noconf = kingpin.Flag("noconf", "Don't watch our own config file").
-	Short('c').
-	Bool()
-
-var beep = kingpin.Flag("bell", "Ring terminal bell if any command returns an error").
-	Short('b').
-	Bool()
-
-var ignores = kingpin.Flag("ignores", "List default ignore patterns and exit").
-	Short('i').
-	Bool()
-
-var doNotify = kingpin.Flag("notify", "Send stderr to system notification if commands error").
-	Short('n').
-	Bool()
-
-var prep = kingpin.Flag("prep", "Run prep commands and exit").
-	Short('p').
-	Bool()
-
-var debug = kingpin.Flag("debug", "Debugging for ppow development").
-	Default("false").
-	Bool()
-
 func main() {
-	kingpin.CommandLine.HelpFlag.Short('h')
-	kingpin.Version(ppow.Version)
-	kingpin.Parse()
+	file := pflag.StringP("file", "f", "ppow.conf", "Path to modfile")
+	noConf := pflag.BoolP("noconf", "c", false, "Don't watch our own config file")
+	beep := pflag.BoolP("bell", "b", false, "Ring terminal bell if any command returns an error")
+	ignores := pflag.BoolP("ignores", "i", false, "List default ignore patterns and exit")
+	doNotify := pflag.BoolP("notifiy", "n", false, "Send stderr to system notification if commands error")
+	prep := pflag.BoolP("prep", "p", false, "Run prep commands and exit")
+	debug := pflag.Bool("debug", false, "Debugging for ppow development")
+	version := pflag.Bool("version", false, "Show application version")
+
+	pflag.Parse()
+
+	if *version {
+		fmt.Println(ppow.Version)
+		os.Exit(0)
+	}
 
 	if *ignores {
 		for _, patt := range ppow.CommonExcludes {
@@ -75,7 +52,7 @@ func main() {
 		notifiers = append(notifiers, &notify.BeepNotifier{})
 	}
 
-	mr, err := ppow.NewModRunner(*file, log, notifiers, !(*noconf))
+	mr, err := ppow.NewModRunner(*file, log, notifiers, !(*noConf))
 	if err != nil {
 		log.Shout("%s", err)
 		return
--- go.mod
+++ go.mod
@@ -1,20 +1,22 @@
 module github.com/dottedmag/ppow
 
-go 1.14
+go 1.22
 
 require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
-	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/cortesi/moddwatch v0.1.0
 	github.com/cortesi/termlog v0.0.0-20210222042314-a1eec763abec
+	github.com/google/go-cmp v0.6.0
+	github.com/spf13/pflag v1.0.5
+)
+
+require (
+	github.com/bmatcuk/doublestar v1.3.4 // indirect
 	github.com/fatih/color v1.13.0 // indirect
-	github.com/google/go-cmp v0.5.9
-	github.com/kr/pretty v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/rjeczalik/notify v0.9.3 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/net v0.4.0 // indirect
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.3.0 // indirect
 )
